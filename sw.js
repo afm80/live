ff8f8962609fa52754034f82ad8c332fa6c4e6ec
Alfaham Net <alfahamnet@gmail.com>
2026-09-26 01:41:20 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+/* Service Worker بسيط لـ PWA */
+var CACHE_NAME = 'alfaham-live-v1';
+var urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './Alfaham-live.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    }).catch(function() {
+      if (event.request.mode === 'navigate') {
+        return caches.match('./index.html');
+      }
+      throw new Error('Network request failed');
+    })
+  );
+});
